293625af9768f0ae0ef8ddc5a819d137ce232445
luuna <luna@hackclub.app>
2025-01-16 11:43:51 +0800

    rewrote

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-import socket
-import threading
+import socket, threading
 from typing import Dict, Tuple
 from utils import *
 
@@ -7,113 +6,69 @@ HOST = '0.0.0.0'
 PORT = 7171
 
 clients: Dict[socket.socket, str] = {}
-running = True
+serverrunning = True
 
 def broadcast(message: bytes, sender: socket.socket = "") -> None:
-    '''
-    broadcasts a message
-    '''
-    for client in clients:
-        if client != sender:
-            try:
-                client.send(f"PUBLIC:{message}\n".encode())
-            except Exception as e:
-                print(f"[SERVER] error sending message {e}")
-                client.close()
-                del clients[client]
-
-def broadcastpriv(message: str) -> None:
     for client in list(clients.keys()):
         try:
-            # TODO: make a random private key each time server starts and send it to the client when client connects
-            client.send(f"PRIVATE:{message}".encode())
+            client.send(f"{message}".encode())
         except Exception as e:
-            print(f"[SERVER] error sending private message {e}")
+            print(f"[SERVER] error sending message {e}")
             client.close()
             del clients[client]
 
-def handleclient(client: socket.socket, addr: Tuple[str, int]) -> None:
-    '''
-    handles communication
-    '''
+def handleclient(client: socket.socket, addr: tuple[str, int]) -> None:
     try:
-        # client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        # username handling
-        username = client.recv(1024).decode().strip()
+        username = client.recv(1024).decode()
         clients[client] = username
-        print(f"{username} joined from {addr}")
-        broadcast(f"[SERVER] {username} joined")
-
-        # message handling
-        client.setblocking(False)
-        buffer = ""
-        while True:
-            try:
-                data = client.recv(1024).decode()
-                if not data:
-                    break
-                buffer += data
-                while "\n" in buffer:
-                    message, buffer = buffer.split("\n", 1)
-                    broadcast(f"{username}: {message}", sender=client)
-                    print(f"{username} sent {message}")
-            except BlockingIOError:
-                pass
-            except Exception as e:
-                print(f"[SERVER] error receiving message {e}")
-                break
-    # cleanup after error
-    finally:
-        if client in clients:
-            username = clients.pop(client, "unknown user")
-            broadcast(f"[SERVER] {username} disconnected", )
-            print(f"{username} disconnected from {addr}")
-    client.close()
-    
-def shutdown(server: socket.socket, reason: str = None) -> None:
-    '''
-    shuts down the server
-    '''
-    global running
-
-    if not reason:
-        print("[SERVER] shutting down...")
-        broadcast("[SERVER] shutting down...")
-    else:
-        print(f"[SERVER] shutting down: {reason}")
-        broadcast(f"[SERVER] shutting down: {reason}")
-    broadcastpriv("quitserver")
-    
-    for client in list(clients.keys()):
+    except Exception as e:
+        print(f"[SERVER] error receiving/setting username")
         client.close()
-    server.close()
+        del clients[client]
 
-    running = False
+    print(f"{username} joined from {addr}")
+    broadcast(f"[SERVER] {username} joined")
 
-    print(f"shut down complete")
-
-def startserver() -> None:
-    '''
-    starts the server (duh)
-    '''
-    
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((HOST, PORT))
-        server.listen()
-        print(f"server started on {HOST}:{PORT}")
-        
+    while True:
         try:
-            while running:
-                client, addr = server.accept()
-                print()
-                threading.Thread(target=handleclient, args=(client,addr), daemon=True).start()
-        except KeyboardInterrupt:
-            shutdown(server)
+            message = client.recv(1024).decode()
+        except Exception as e:
+            print(f"[SERVER] error receiving message")
+            client.close()
+            del clients[client]
 
-# TODO: add support for custom ip and port
-# TODO: add system commands, sending messages, etc
-# input("start server: ")
+        broadcast(f"{username}: {message}", sender=client)
+
+    
+def startserver() -> None:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind((HOST, PORT))
+            server.listen()
+
+            print(f"server started on {HOST}:{PORT}")
+
+            while True:
+                client, addr = server.accept()
+                threading.Thread(target=handleclient, args=(client, addr), daemon=True).start()
+    except KeyboardInterrupt:
+        print(f"shutdown starting")
+
+        for client in list(clients.keys()):
+            print(f"informing client that server is shutting down")
+            broadcast(f"[SERVER] shutting down...")
+            print(f"closing connection with {clients[client]}")
+            client.close()
+            print(f"removing {clients[client]} from online clients")
+            del clients[client]
+            print(f"done")
+            print()
+        print(f"server shutting down")
+
+
+# # TODO: add support for custom ip and port
+# # TODO: add system commands, sending messages, etc
+# # input("start server: ")
 print("starting server")
 print()
 startserver()
