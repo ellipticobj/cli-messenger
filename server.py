443fc7d364373a5c2f9feee77c7d31c6eabe43b7
luuna <luna@hackclub.app>
2025-02-06 09:27:59 +0800

    server funcs

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket
 import threading
 import time
 from datetime import datetime
+from typing import _P
+
+from client import Client
 
 class Server:
     def __init__(self, host='0.0.0.0', port=7171):
@@ -23,3 +26,34 @@ class Server:
         self.profanity_words = set(['badword1', 'badword2'])
 
         # self.setup() TODO
+
+    def setup(self):
+        '''sets up the server'''
+        pass
+
+    def log(self, message):
+        '''prints a log + stores it in a log file'''
+        pass
+
+    def handleclient(self, clsock):
+        '''handles client connection'''
+        pass
+
+    def validatecl(self, clip):
+        '''checks if client's ip is black/whitelisted'''
+        pass
+
+    def broadcast(self, message):
+        for client in self.clients:
+            try:
+                client.send(message.encode())
+            except Exception as e:
+                self.log(f"failed to send message to {client} due to {e}")
+
+if __name__ == "__main__":
+    server = Server()
+
+    acceptthread = threading.Thread(target = lambda: [server.handleclient(client) for client in iter(lambda: server.server.accept()[0], None)], daemon=True)
+    acceptthread.start()
+
+    print(f"server started at {server.host}:{server.port}")
